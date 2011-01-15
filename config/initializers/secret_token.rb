db6cbb55bf04922dffe558b5d8214aa07b42a649
mskwarcan <mike@balcomagency.com>
2011-01-14 19:15:37 -0600

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Refinery::Application.config.secret_token = '849145684ff776e792f507cf85fb255f9ded0e11c7ff8788b7cdbb26e73c564df10d757db1ab8a6687edf88befdb8f15370c8b441dbffbe43c5131c3d4b9888f'
