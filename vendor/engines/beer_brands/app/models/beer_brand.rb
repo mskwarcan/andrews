037ba83555418a197a9665b405557356efc91ae5
mskwarcan <mike@balcomagency.com>
2011-01-20 19:05:10 -0600

    brands model

--- vendor/engines/beer_brands/app/models/beer_brand.rb
+++ vendor/engines/beer_brands/app/models/beer_brand.rb
@@ -3,8 +3,6 @@ class BeerBrand < ActiveRecord::Base
   acts_as_indexed :fields => [:main_category, :sub_category]
   
   validates_presence_of :main_category
-  validates_uniqueness_of :main_category
-  
   belongs_to :image
 
 
