b5d9a9e477b0d8aa9ca2dec315516c64bfc21c34
mskwarcan <mike@balcomagency.com>
2011-01-21 19:39:19 -0600

    Regions to form

--- app/mailers/donation_mailer.rb
+++ app/mailers/donation_mailer.rb
@@ -3,6 +3,8 @@ class DonationMailer < ActionMailer::Base
   
   def request_email(donation_request)
     @donation = donation_request
-    mail(:to => "mike@balcomagency.com, mskwarca@purdue.edu", :subject => "Donation Request Form Entry" )
+    
+    region = Region.first(:conditions => {:region => @donation.region})
+    mail(:to => region.email, :subject => "Donation Request Form Entry" )
   end
 end
