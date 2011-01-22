b3dc0d0a84947bd8ff352820454803fcfbf2a771
mskwarcan <mike@balcomagency.com>
2011-01-21 19:44:10 -0600

    fix

--- app/mailers/donation_mailer.rb
+++ app/mailers/donation_mailer.rb
@@ -4,7 +4,7 @@ class DonationMailer < ActionMailer::Base
   def request_email(donation_request)
     @donation = donation_request
     
-    region = Region.first(:conditions => {:region => @donation.event_region})
+    region = Region.first(:conditions => {:title => @donation.event_region})
     
     mail(:to => region.email, :subject => "Donation Request Form Entry" )
   end
