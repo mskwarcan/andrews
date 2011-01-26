5cdccdc2f13c10c423fb4c0d3daae7ad130994ab
mskwarcan <mike@balcomagency.com>
2011-01-25 18:48:00 -0600

    vimeo migration

---
+++ db/migrate/20110126004512_vimeo.rb
@@ -0,0 +1,18 @@
+class Vimeo < ActiveRecord::Migration
+  def self.up
+    add_column :for_brewers, :vimeo, :text
+    add_column :for_retailers, :vimeo, :text
+    remove_column :for_brewers, :ogg_file
+    remove_column :for_retailers, :ogg_file
+    remove_column :for_brewers, :m4v_file
+    remove_column :for_retailers, :m4v_file
+    remove_column :for_brewers, :display_image
+    remove_column :for_retailers, :display_image
+    
+  end
+
+  def self.down
+    remove_column :for_brewers, :vimeo, :text
+    remove_column :for_retailers, :vimeo, :text
+  end
+end
